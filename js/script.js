1a01895781f9947ddd4f440ad6e75148f692bcd2
Abfields10 <abbifields@gmail.com>
2025-04-30 20:11:19 -0400

    pics

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ const succulent = document.querySelector("#succulent");
 succulent.onclick = function () {
   alert("You selected succulent!");
 };
+
+<button onclick="window.location.href='https://www.example.com';">
+  Click Me
+</button>;
